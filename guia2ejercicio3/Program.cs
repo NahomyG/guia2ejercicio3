dac71a4d241cf5b4275b7b3f7bc7a887d02db19e
NahomyG <114265537+NahomyG@users.noreply.github.com>
2023-11-13 16:23:52 -0600

    commit 7

---
+++ guia2ejercicio3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guia2ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un numero: ");
+            int numero = int.Parse(Console.ReadLine());
+            if (numero % 2 == 0)
+            {
+                Console.Write("El numero " + numero + " es Par.");
+            }
+            else
+            {
+                Console.Write("El numero " + numero + " es Impar.");
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
